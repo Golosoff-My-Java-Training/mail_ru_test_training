2d26ff19c7df9212d2b265be04ab3697ba632281
Golosoff Pavel <golosoff@gmail.com>
2017-07-24 14:29:55 +0300

    1

---
+++ src/main/java/by/htp/mail_ru_test/App.java
@@ -0,0 +1,13 @@
+package by.htp.mail_ru_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
